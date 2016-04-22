b7d6937300c5fc98bd79785793c8265c97837fc6
Agnieszka Kurek <agniechakurek@gmail.com>
2016-04-22 19:28:06 +0200

    added content and decoration  page top, integrated component, and added smooth scrolling and parallax scrolling

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -6,7 +6,18 @@ $(function () {
 
     // add Bootstrap's scrollspy
     $('body').scrollspy({
-        target: '.navbar'
+        target: '.navbar',
+         offset: 160
     });
+    // smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // parallax scrolling with stellar.js
+    $(window).stellar();
 
 });
